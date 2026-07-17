8e6aa57c68e7f8517ddf181f860e0e76226ba2ac
Aryan <av028679@gmail.com>
2026-07-17 20:12:49 +0530

    Fix Vercel build

--- Frontend/src/Features/auth/services/auth.api.js
+++ Frontend/src/Features/auth/services/auth.api.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
-    withCredentials:true
-})
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
 
-export async function register({username,email,password}){
-    try {
-        const response = await api.post('/api/auth/register',{
-            username,email,password
-        })
+export default api;
+
+    export async function register({username,email,password}){
+        try {
+            const response = await api.post('/api/auth/register',{
+                username,email,password
+            })
 
         return response.data
     } catch (error) {
