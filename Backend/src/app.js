39bd001cf623d31e4103a51b0a418995c869bec0
Aryan <av028679@gmail.com>
2026-07-17 21:50:09 +0530

    Fix CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,8 +8,8 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
+    origin:["http://localhost:5173","https://interview-resume-five.vercel.app"],
+    withCredentials:true
 }))
 app.use('/api/auth',authRouter)
 app.use('/api/interview',InterviewRouter)
